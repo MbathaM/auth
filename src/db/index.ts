067213a4169bc605ce4c3b36e3c06a08c3dfbd1e
MbathaM <33201125+MbathaM@users.noreply.github.com>
2025-05-21 13:31:36 +0200

    Update index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,4 +4,6 @@ import { createClient } from '@libsql/client';
 import * as schema from './schema';
 
 const client = createClient({ url: process.env.DB_FILE_NAME! });
-export const db = drizzle({ client, schema, logger: true });
+const db = drizzle({ client, schema, logger: true });
+
+export { db, schema };
